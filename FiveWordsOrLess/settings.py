454f1194f97312ad1c3060e6465a9fb6cc165318
aplassage <andrew.plassard@gmail.com>
2013-06-01 10:57:19 -0400

    I can't spell

--- FiveWordsOrLess/settings.py
+++ FiveWordsOrLess/settings.py
@@ -2,14 +2,24 @@
 import os
 PROJECT_ROOT = os.path.abspath(os.path.dirname(__file__))
 
-DEBUG = Tre
+DEBUG = True
 TEMPLATE_DEBUG = DEBUG
 
 ADMINS = (
     # ('Your Name', 'your_email@example.com'),
 )
 
-DATABASES={}
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': '',                      # Or path to database file if using sqlite3.
+        # The following settings are not used with sqlite3:
+        'USER': '',
+        'PASSWORD': '',
+        'HOST': '',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
+        'PORT': '',                      # Set to empty string for default.
+    }
+}
 
 MANAGERS = ADMINS
 
@@ -155,7 +165,7 @@ LOGGING = {
 
 try:
     from settings_dev import *
-    print 'It looks like a production environment'
+    print 'It looks like a testing environment'
 except ImportError:
     print 'Well we are in a development environment'
     import dj_database_url
